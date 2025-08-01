e75450452c66126543251edd80a55b1280c052ee
Johnden Llatuna <jllatuna30115@liceo.edu.ph>
2025-08-01 11:28:19 +0800

    lab-activity-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static int number = 0;
+    static int totalNumber = 0;
+
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 1; i <= 5; i++){
+            System.out.print("Enter Number " + i + ": ");
+            number = scanner.nextInt();
+            totalNumber = totalNumber + number;
         }
+
+        System.out.println("\nTotal Number: " + totalNumber);
+        scanner.close();
     }
+}
