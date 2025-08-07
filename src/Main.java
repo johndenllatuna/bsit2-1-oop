a128f3af0063a145d09789e1cffed3e1139b4fb8
Johnden Llatuna <jllatuna30115@liceo.edu.ph>
2025-08-07 10:42:15 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
-        }
+        Student s1 = new Student("Johnden", 19, "Information Technology", 90, 90, 90);
+        s1.displayInfo();
+
+        Student s2 = new Student("Lauron", 20, "Information Technology", 85, 90, 86);
+        s2.displayInfo();
+
+        Student s3 = new Student("Lactuan", 22, "Information Technology", 70, 71, 68);
+        s3.displayInfo();
+
+        System.out.println("\nSummary: " + Student.passingStudents + " out of " + Student.totalStudents + " students are passing");
     }
+}
