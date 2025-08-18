d45b70afbdc23adec81bc2b086d7514175c19ae8
Johnden Llatuna <jllatuna30115@liceo.edu.ph>
2025-08-18 10:10:26 +0800

    lab-activity-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        GradeCalculator gc = new GradeCalculator();
+
+        String studentName = "Johnden Llatuna";
+        double average = gc.calculateAverage(89.5, 87.0, 94.5, 92.9);
+        String letterGrade = gc.getLetterGrade(average);
+
+        gc.displayResult(studentName, average);
+        gc.displayResult(studentName, average, letterGrade);
         }
     }
