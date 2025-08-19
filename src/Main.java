adb0af748aeecbf5577ef7c59071fc05f4fff242
Johnden Llatuna <jllatuna30115@liceo.edu.ph>
2025-08-19 11:51:34 +0800

    lab-activity-5

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        GradeCalculator gc = new GradeCalculator();
+        System.out.println("Bank Name: " + BankAccount.bankName);
+        System.out.println("Interest Rate: " + (BankAccount.interestRate * 100) + "%\n");
 
-        String studentName = "Johnden Llatuna";
-        double average = gc.calculateAverage(89.5, 87.0, 94.5, 92.9);
-        String letterGrade = gc.getLetterGrade(average);
+        BankAccount acc1 = new BankAccount("John Den", 1200);
+        BankAccount acc2 = new BankAccount("Jose Manuel", 2300);
+        BankAccount acc3 = new BankAccount("Ashton Lactuan", 1500);
 
-        gc.displayResult(studentName, average);
-        gc.displayResult(studentName, average, letterGrade);
-        }
+        System.out.println("\n═══ Account Operations ═══");
+        acc1.deposit(500);
+        acc2.withdraw(300);
+
+        System.out.println("\n═══ Interest Calculation ═══");
+        System.out.println("John Den's interest: $" + acc1.calculateInterest());
+        System.out.println("Jose Manuel's interest: $" + acc2.calculateInterest());
+        System.out.println("Ashton Lactuan's interest: $" + acc3.calculateInterest());
+
+        System.out.println("\nTotal Accounts Created: " + BankAccount.totalAccounts);
     }
+}
